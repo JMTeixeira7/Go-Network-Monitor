87cac75e7406ef8c1c38b44607723e0da8edc859
Joao Teixeira <joaomcateixeira@tecnico.ulisboa.pt>
2026-03-03 23:47:14 +0100

    feat: Implement database phishing service

--- internal/db/databaseService/phishingDBService/phishing_db.go
+++ internal/db/databaseService/phishingDBService/phishing_db.go
@@ -3,6 +3,7 @@ package phishingDBService
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
 	"github.com/JMTeixeira7/Go-Network-Monitor.git/internal/model"
 )
@@ -17,10 +18,71 @@ func NewPhishingDBService(db *sql.DB) *PhishingDBService {
 	}
 }
 
-func (p *PhishingDBService) CheckForPhishing(ctx context.Context, cred *model.Credentials, domain string) (bool, string, error) {
-	panic("not yet implemented")
+func (p *PhishingDBService) CheckForPhishing(ctx context.Context, cred *model.Credentials, domain string) (bool, *string, error) {
+	phishing, err := fetchPhishingInstance(ctx, cred, domain, p.db)
+	if err != nil {
+		return false, nil, fmt.Errorf("Failed to fetch credentials instance from database: %s", err)
+	}
+	if phishing != nil {
+		return true, phishing, nil
+	}
+	return false, nil, nil
 }
 
 func (p *PhishingDBService) PushCredentials(ctx context.Context, cred *model.Credentials, domain string) error {
-	panic("not yet implemented")
+	const q = `
+		INSERT INTO credentials (domain_key, username, fingerprint)
+		VALUES (?, ?, ?)
+	`
+	
+	domain_key, err := fetchDomainIDInstance(ctx, domain, p.db)
+	if err != nil {
+		return fmt.Errorf("Failed to push credentials into table:\n %s", err)
+	}
+	_, err = p.db.ExecContext(ctx, q, domain_key, cred.Username, cred.Fingerprint)
+	if err != nil {
+		return fmt.Errorf("push domain: %w", err)
+	}
+	return nil
+}
+
+func fetchPhishingInstance(ctx context.Context, cred *model.Credentials, domain string, db *sql.DB) (*string, error) {
+	const q = `
+		SELECT v.domain
+		FROM credentials c
+		JOIN visitedDomains v ON v.id = c.domain_key
+		WHERE c.username = ? AND c.fingerprint = ? AND v.domain <> ?
+		ORDER BY v.time DESC
+		LIMIT 1;
+	`
+
+	var legitDomain string
+	err := db.QueryRowContext(ctx, q, cred.Username, cred.Fingerprint, domain).Scan(&legitDomain)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &legitDomain, nil
+}
+
+func fetchDomainIDInstance(ctx context.Context, domain string, db *sql.DB) (*int, error) {
+
+	const q = `
+		SELECT id
+		FROM visitedDomains
+		WHERE domain = ?
+		LIMIT 1
+	`
+	var domain_key int
+	err := db.QueryRowContext(ctx, q, domain).Scan(&domain_key)
+	if err != sql.ErrNoRows {
+		return nil, fmt.Errorf("Could not fetch a domain instance for the given credentials (Unwanted behaviour): %s", domain)
+	}
+	if err != nil {
+		return  nil, err
+	}
+	return &domain_key, nil
+		
 }
--- internal/db/init.sql
+++ internal/db/init.sql
@@ -13,3 +13,21 @@ VALUES
   ('github.com', NULL),
   ('gmail.com', NULL),
   ('lirias2.kuleuven.be', NULL);
+
+DROP TABLE IF EXISTS credentials;
+CREATE TABLE credentials (
+  id          INT AUTO_INCREMENT NOT NULL,
+  domain_key  INT NOT NULL,
+  username    VARCHAR(255) NOT NULL,
+  fingerprint VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+  FOREIGN KEY (`domain_key`) REFERENCES visitedDomains(`id`)
+)
+
+INSERT INTO credentials
+  (domain_key, username, fingerprint)
+VALUES
+  (1, 'user1', 'example1')
+  (2, 'user2', 'example2')
+
+/*TODO: Add constraint that, domain_key is UNIQUE, and overwrite attempt should change last credentials */
